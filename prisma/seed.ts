33ba7ebe218870077d38690b5b3a83c1090a69ce
Rj-Aquino <richardjasonmaquino@iskolarngbayan.pup.edu.ph>
2025-05-27 22:24:03 +0800

    Changes for Deployment

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { generateFormattedID } from '../lib/idGenerator'
 
 const prisma = new PrismaClient();
