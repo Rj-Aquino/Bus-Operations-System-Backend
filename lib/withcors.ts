46bdc341b3e1a26d01ae0fd993535e32354c71ab
Yuan Evangelista <yuanexequielevangelista03@gmail.com>
2025-06-18 07:10:00 +0800

    nilagay ko lang yung localhost ko

--- lib/withcors.ts
+++ lib/withcors.ts
@@ -9,6 +9,7 @@ const allowedOrigins = [
   'http://192.168.0.161:3000',
   'https://boms.agilabuscorp.me',
   'http://192.168.0.168:3000',
+  'http://192.168.0.132:3000',
 ];
 
 export function withCors(handler: (req: NextRequest) => Promise<NextResponse>) {
